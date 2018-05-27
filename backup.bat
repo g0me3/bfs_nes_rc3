1f6746a42c9a559c05893257b8150f606692ff14
cah4e3 <cah4e3@gmail.com>
2018-05-27 13:16:01 +0300

    160114_0 initial original clean source

---
+++ backup.bat
@@ -0,0 +1,11 @@
+mkdir .\backup\current
+copy /b
+
+copy /b make.bat .\backup\current\make.bat
+copy /b backup.bat .\backup\current\backup.bat
+copy /b *.inc .\backup\current\*.inc
+copy /b *.exe .\backup\current\*.exe
+copy /b *.nas .\backup\current\*.nas
+copy /b !*.chr .\backup\current\!*.chr
+copy /b copyrights.txt .\backup\current\copyrights.txt
+copy /b notes.txt .\backup\current\notes.txt
---
+++ make.bat
@@ -0,0 +1,41 @@
+@echo off
+
+start "" "..\..\..\tools\TextPad 5\TextPad.exe" bank0.inc bank1.inc bank2.inc bank3.inc bank4.inc bank5.inc bank6.inc bank7.inc ram.inc notes.txt
+
+pause 0
+
+:again
+
+del !err.log
+del !rc3.nes
+del !rc3.hdr
+
+echo assembling...
+
+dasm bank0.nas -f3 -obank0.bin
+dasm bank0.nas -f3 -l!rc3.lst  -obank0.bin >> !err.log
+
+for %%f in (*.nas) do call :dodasm %%f > NUL
+
+dasm hdr.nas -f3 -o!rc3.hdr > NUL
+
+goto :cleanup
+
+:dodasm
+dasm %1 -f3 -o%~n1.bin
+goto :eof
+
+:cleanup
+echo build...
+copy /b bank0.bin+bank1.bin+bank2.bin+bank3.bin+bank4.bin+bank5.bin+bank6.bin+bank7.bin !rc3.prg > NUL
+nesimage j !rc3 > NUL
+
+echo cleanup...
+rem for %%f in (*.bin) do del %%f
+
+echo done.
+pause 0
+
+goto :again
+
+:eof
